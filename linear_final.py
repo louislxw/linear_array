7e67b3cb51824be55a05d15b73e8b1483f3c2cde
louislxw <louislxw@gmail.com>
2020-05-17 17:28:14 +1000

    Adding instruction level DFT&FFT methods

--- linear_final.py
+++ linear_final.py
@@ -1,6 +1,33 @@
+import time
+import numpy as np
 from queue import deque
 from queue import Queue
 from scipy.fftpack import fft, fftshift
+
+
+def DFT(x):
+    """Compute the Discrete Fourier Transform of the 1D array x"""
+    N = len(x)
+    n = np.arange(N)
+    k = n.reshape((N, 1))
+    M = np.exp(-2j * np.pi * k * n / N)
+    return np.dot(M, x)
+
+
+def FFT(x):
+    """A recursive implementation of the 1D Cooley-Turkey FFT"""
+    N = len(x)
+
+    if N % 2 > 0:
+        raise ValueError("size of x must be a power of 2")
+    elif N <= 32:  # this cutoff should be optimized
+        return DFT(x)
+    else:
+        X_even = FFT(x[::2])
+        X_odd = FFT(x[1::2])
+        factor = np.exp(-2j * np.pi * np.arange(N) / N)
+        return np.concatenate([X_even + factor[:N // 2] * X_odd,
+                               X_even + factor[N // 2:] * X_odd])
 
 
 class LinearArrayCell:
@@ -35,25 +62,29 @@ class LinearArrayCell:
                 else:
                     self.single_in = self.cell_input.get()
                 self.data_to_compute_1.put(self.single_in)
-                self.data_to_compute_2.put(self.single_in.real - self.single_in.imag*1j)  # conjugate
+                self.data_to_compute_2.put(self.single_in.real - self.single_in.imag * 1j)  # conjugate
         else:
             for _ in range(self.cell_size):
                 self.single_in = self.cell_input.cell_shift.get()
                 self.data_to_compute_1.put(self.single_in)
-                # self.data_to_compute_2.put(self.single_in.real - self.single_in.imag*1j)
+                # self.data_to_compute_2.put(self.single_in.real - self.single_in.imag * 1j)
 
-    def compute(self):
+    def compute(self, iterations):
         list_1 = list(self.data_to_compute_1.queue)
         list_2 = list(self.data_to_compute_2.queue)
         list_3 = []
         for i in range(len(list_1)):
             real_part = list_1[i].real * list_2[i].real - list_1[i].imag * list_2[i].imag
             image_part = list_1[i].imag * list_2[i].real + list_1[i].real * list_2[i].imag
-            self.cell_partial_result = real_part + image_part * 1j
+            self.cell_partial_result = real_part + image_part * 1j  # result of conjugate multiplication
             list_3.append(self.cell_partial_result)
             # self.cell_output.put(self.cell_partial_result)
-        fft_results = fft(list_3)
-        fft_shift_results = fftshift(fft_results)
+        # dft_result = DFT(list_3)
+        fft_result = FFT(list_3)
+        # fftpack_result = fft(list_3)
+        # print(f'Compare DFT with built-in FFT at PE {iterations}:', np.allclose(DFT(list_3), fft(list_3)))
+        # print(f'Compare FFT with built-in FFT at PE {iterations}:', np.allclose(FFT(list_3), fft(list_3)))
+        fft_shift_results = fftshift(fft_result)
         self.cell_output.queue = deque(fft_shift_results)
 
         for _ in range(self.cell_size):
@@ -89,9 +120,9 @@ class LinearArray:
 
     def compute(self):
         for cell in self.cells:
-            cell.compute()
+            cell.compute(self.iterations)
             for _ in range(cell.cell_size):
-                self.result[cell.signal_index-1][cell.cell_index].put(cell.cell_output.get())
+                self.result[cell.signal_index - 1][cell.cell_index].put(cell.cell_output.get())
 
     def run(self, total_iterations):
         for _ in range(total_iterations):
@@ -106,27 +137,36 @@ signals = 1
 pes = 4
 registers = 32
 input_queue = [[Queue() for _ in range(pes)] for _ in range(signals)]
+read_cycle = registers * pes
+compute_cycle = registers + registers * np.log2(registers)
+shift_cycle = registers
+total_cycle = read_cycle + compute_cycle + (shift_cycle + compute_cycle) * (pes - 1)
+print(f'No. of cycles = {total_cycle}, Execution time = {total_cycle*2/1000} us.')
 
 for signal in range(signals):
     for pe in range(pes):
         if signal == 0:
             for index in range(pe * registers, (pe + 1) * registers):
-                complex_data = index/128 + (index+1)/128 * 1j
+                complex_data = index / 128 + (index + 1) / 128 * 1j
                 input_queue[signal][pe].put(complex_data)
         if signal > 0:
             for index in reversed(range(pe * registers, (pe + 1) * registers)):
-                complex_data = index/128 + (index+1)/128 * 1j
+                complex_data = index / 128 + (index + 1) / 128 * 1j
                 input_queue[signal][pe].put(complex_data)
 
 # print(list(input_queue[0][-1].queue))
 
 myArray = LinearArray(pes, registers, input_queue)
-res = myArray.run(signals*pes)  # run (signal*pes) times
+start_time = time.time()
+res = myArray.run(signals * pes)  # run (signal*pes) times
+end_time = time.time()
+print(f'---{end_time - start_time} seconds ---')
 
+'''
 for i in range(signals):
     for j in range(pes):
         # print('PE%d:' %j, list(res[i][j].queue))
-        print('PE{:d} output: {}'.format(j, ['%.5f, %.5f' % (element.real, element.imag) for element in list(res[i][j].queue)]))
+        # print('PE{:d} output: {}'.format(j, ['%.5f, %.5f' % (element.real,
+        # element.imag) for element in list(res[i][j].queue)]))
         print(len(list(res[i][j].queue)))
-
-
+'''
